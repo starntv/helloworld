f3876a8348db05230e07d89873daec8e605cf4c1
starntv <104955178+starntv@users.noreply.github.com>
2022-11-05 09:28:12 +0530

    Update world.java

--- world.java
+++ world.java
@@ -1,4 +1,7 @@
-public static void main{
-  
-  system.out.println("hello world");
+// Your First Program
+
+class world
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
